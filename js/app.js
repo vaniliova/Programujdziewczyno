03e7e4ca8553f32a4ac8831aff0a7cf0a6dd98c2
Dagmara Lewandowska <vanila.dl@gmail.com>
2017-10-14 16:19:39 +0200

    exercise 2

--- js/app.js
+++ js/app.js
@@ -10,9 +10,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
       forDropdown.addEventListener('mouseover', function() {
       	menu.style.display = "block";
-      })
+      });
 
       forDropdown.addEventListener('mouseout', function() {
       	menu.style.display = "none";
-      })
+      });
+
+
+      var buttons = document.querySelectorAll('.read-more');
+
+      	function showHide() {
+	      	var textArea = this.previousElementSibling;
+	      	if(textArea.style.display === 'none' || textArea.style.display === '') {
+	      		textArea.style.display = 'block';
+	      		this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+	      	}else{
+	      		textArea.style.display = 'none';
+	      		this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+	      	}
+      	}
+
+      	for(var i = 0; i<= buttons.length-1; i++) {
+      		buttons[i].addEventListener('click', showHide);
+      	}
 });
